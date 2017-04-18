808de5750ec81b8bdb9925f5f7ec3ab0c72f34f8
Gerald Condon <gerald@geraldcondon.com>
2017-04-18 17:53:02 +0000

    fixed

--- pca9685v01.js
+++ pca9685v01.js
@@ -82,8 +82,7 @@ PCA9685.prototype.moveServo = function (num, val) {
   if (num < 1 || num > 16) {
     throw "Servos are 1-indexed. Servos can be between 1-16.";
   }
-  console.log(1,Math.ceil(800+(1400)*(val/180)));
-  this.setPWM(num,1,Math.ceil(800+(1400)*(val/180)));
+  this.setPWM(num,(((val/180) * (this.high - this.low)) + this.low), Math.floor(Constants.MAX/100*(((val/180) * (this.high - this.low)) + this.low)));
 };
 //I2C1.setup({scl:B8, sda:B9});
 //var driver = connect(I2C1, 50);
